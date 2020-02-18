e265b9f9f7794741e77f145ea95cc4423b6ebbeb
Pratyusha Chirravuri <Pratyusha_Chirravuri@epam.com>
2020-02-18 16:52:43 +0530

    Add project files.

---
+++ WebApplication1/Controllers/PosttController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PosttController : Controller
+    {
+        // GET: Postt
+        private EmployeeEntities db = new EmployeeEntities();
+        public ActionResult Index()
+        {
+            return View(db.emp_table.ToList());
+        }
+
+        // GET: Postt/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Postt/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Postt/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                emp_table e = new emp_table();
+             
+                e.empName = collection["empName"];
+                e.empAddress = collection["empAddress"];
+                e.empPhoneNo = collection["empPhoneNo"];
+                EmployeeEntities userRegistrationEntities = new EmployeeEntities();
+                userRegistrationEntities.emp_table.Add(e);
+                userRegistrationEntities.SaveChanges();
+                return RedirectToAction("Create");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Postt/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Postt/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Postt/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Postt/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/emp_tableController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class emp_tableController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: emp_table
+        public ActionResult Index()
+        {
+            return View(db.emp_table.ToList());
+        }
+
+        // GET: emp_table/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp_table emp_table = db.emp_table.Find(id);
+            if (emp_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp_table);
+        }
+
+        // GET: emp_table/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: emp_table/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "empID,empName,empAddress,empPhoneNo")] emp_table emp_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.emp_table.Add(emp_table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(emp_table);
+        }
+
+        // GET: emp_table/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp_table emp_table = db.emp_table.Find(id);
+            if (emp_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp_table);
+        }
+
+        // POST: emp_table/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "empID,empName,empAddress,empPhoneNo")] emp_table emp_table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emp_table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp_table);
+        }
+
+        // GET: emp_table/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            emp_table emp_table = db.emp_table.Find(id);
+            if (emp_table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emp_table);
+        }
+
+        // POST: emp_table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            emp_table emp_table = db.emp_table.Find(id);
+            db.emp_table.Remove(emp_table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
